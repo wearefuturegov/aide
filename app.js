44e177b8876552815df2546b04c0c1bbe5881b58
pezholio <pezholio@gmail.com>
2018-02-19 19:32:48 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+
+var bot = require('./lib/bot');
+app.post('/api/messages', bot.listen());
+
+// Error handlers
+
+// Development error handler, will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+    console.log(err.message)
+  });
+}
+
+// Production error handler, no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+// Start listening
+var port = process.env.port || process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Web Server listening on port %s', port);
+});
