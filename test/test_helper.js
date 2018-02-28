387c6dadb82f602b2782a9fe515b27899338df58
pezholio <pezholio@gmail.com>
2018-02-28 16:01:13 +0000

    Record HTTP requests

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 
 var botBuilder = require('botbuilder');
 var botTester = require('bot-tester');
+var replay  = require('replay');
+
+replay.fixtures = __dirname + '/fixtures';
 
 var connector = new botTester.TestConnector({
   defaultAddress: botBuilder.IAddress
