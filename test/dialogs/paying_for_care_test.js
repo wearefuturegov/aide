ff6b1095a57cc8319f20be47a3c3a5a46180eac8
pezholio <pezholio@gmail.com>
2018-03-12 09:33:39 +0000

    Remove assessment and show information instead

--- test/dialogs/paying_for_care_test.js
+++ test/dialogs/paying_for_care_test.js
@@ -2,7 +2,7 @@ var helper = require('../test_helper');
 var dialogs = require('../../lib/dialogs')
 var t = require('../../lib/config')
 
-describe('intro', function() {
+describe('paying for care', function() {
   let bot;
   let botTester;
   this.timeout(15000);
@@ -19,46 +19,12 @@ describe('intro', function() {
     return botTester
       .sendMessageToBot(
         'hello',
-        t.payingforcare.info_1
+        t.payingforcare.info_1,
+        t.payingforcare.info_2,
+        t.payingforcare.info_3,
+        t.payingforcare.info_4
       )
       .runTest();
-  })
-  
-  context('asks me if I want to start an assessment', function() {
-    
-    beforeEach(function() {
-      botTester
-        .sendMessageToBot(
-          'hello',
-          t.payingforcare.info_1
-        )
-    })
-    
-    context('yes', function() {
-      
-      it('forwards me to starting an assessment', function() {
-        return botTester
-          .sendMessageToBot(
-            'yes',
-            t.assessment.intro,
-            t.assessment.first_name
-          ).runTest();
-      })
-      
-    })
-    
-    context('no', function() {
-      
-      it('closes the conversation', function() {
-        return botTester
-          .sendMessageToBot(
-            'no',
-            t.end_conversation
-          ).runTest();
-      })
-      
-    })
-    
   })
   
 })
