df2efd7f8a4a774819445968e64463175e1e03d0
pezholio <pezholio@gmail.com>
2018-03-15 17:51:26 +0000

    Detect equipment types at the start of the conversation

--- test/dialogs/help_chooser_test.js
+++ test/dialogs/help_chooser_test.js
@@ -12,7 +12,8 @@ describe('intro', function() {
       '/': dialogs.help_chooser,
       'confirm': dialogs.confirm,
       'equipmentType': dialogs.equipmentType,
-      'payingforcare': dialogs.payingForCare
+      'payingforcare': dialogs.payingForCare,
+      'confirmEquipment': dialogs.confirmEquipment
     })
     botTester = new helper.botTester.BotTester(bot)
   });
@@ -43,11 +44,87 @@ describe('intro', function() {
       .runTest();
   });
   
+  describe('equipment', function() {
+  
+    it('help in the bathroom', function() {
+      return botTester
+        .sendMessageToBot(
+          'Getting on the toilet',
+          t.equipment.confirm.replace('%s', 'in the bathroom')
+        )
+        .runTest();
+    })
+  
+    it('help in the bedroom', function() {
+      return botTester
+        .sendMessageToBot(
+          'Getting out of bed',
+          t.equipment.confirm.replace('%s', 'in the bedroom')
+        )
+        .runTest();
+    })
+  
+    it('help in the dining room', function() {
+      return botTester
+        .sendMessageToBot(
+          'Eating my dinner',
+          t.equipment.confirm.replace('%s', 'in the dining room')
+        )
+        .runTest();
+    })
+  
+    it('help in the garden', function() {
+      return botTester
+        .sendMessageToBot(
+          'mowing the lawn',
+          t.equipment.confirm.replace('%s', 'in the garden')
+        )
+        .runTest();
+    })
+  
+    it('help in the office', function() {
+      return botTester
+        .sendMessageToBot(
+          'reading and writing',
+          t.equipment.confirm.replace('%s', 'in the office')
+        )
+        .runTest();
+    })
+  
+    it('help in the kitchen', function() {
+      return botTester
+        .sendMessageToBot(
+          'cooking my dinner',
+          t.equipment.confirm.replace('%s', 'in the kitchen')
+        )
+        .runTest();
+    })
+  
+    it('help in the lounge', function() {
+      return botTester
+        .sendMessageToBot(
+          'getting out of my chair',
+          t.equipment.confirm.replace('%s', 'in the lounge')
+        )
+        .runTest();
+    })
+  
+    it('help on the stairs', function() {
+      return botTester
+        .sendMessageToBot(
+          'getting up the stairs',
+          t.equipment.confirm.replace('%s', 'on the stairs')
+        )
+        .runTest();
+    })
+  
+  })
+  
   it('Tells me it can\'t understand', function() {
     return botTester
       .sendMessageToBot(
-        'Something',
-        'Sorry, I did not understand \'Something\'. Please try rephrasing your request'
+        'Whatever',
+        'Sorry, I did not understand \'Whatever\'. Please try rephrasing your request'
       )
       .runTest();
   });
@@ -55,11 +132,11 @@ describe('intro', function() {
   it('Presents me with options the second time round', function() {
     return botTester
       .sendMessageToBot(
-        'Something',
-        'Sorry, I did not understand \'Something\'. Please try rephrasing your request'
+        'Whatever',
+        'Sorry, I did not understand \'Whatever\'. Please try rephrasing your request'
       )
       .sendMessageToBot(
-        'Something',
+        'Whatever',
         t.help_chooser.options
       )
       .runTest();
